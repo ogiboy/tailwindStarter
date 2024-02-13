edf7caff5f4218bf8ce80fd9b5e31e00c53536ae
Oğuzcan <131633350+ogiboy@users.noreply.github.com>
2024-02-14 01:16:32 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+// App/Header/Form bileşenlerine ayırın, kendi adlarıyla ayrı dosyalara koyun
